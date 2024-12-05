6dbc88d591273a7345a49c2f98ecc98efaa2532c
Bingfan Xu <ec21635@qmul.ac.uk>
2024-12-05 22:05:58 +0000

    Update CarbonCredit.sol

--- backend/contracts/CarbonCredit.sol
+++ backend/contracts/CarbonCredit.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.28;
 
+import "./IERC20.sol";
 contract CarbonCredit is IERC20{
   // specific token for carbon credit
   uint256 supply;
