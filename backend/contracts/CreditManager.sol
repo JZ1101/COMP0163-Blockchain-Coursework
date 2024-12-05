4dccd5e51f5c0a878d6d4f868f4c15d0bc653769
Jianian Zheng <jnz7276@gmail.com>
2024-12-05 21:45:09 +0000

    Update CreditManager.sol

--- backend/contracts/CreditManager.sol
+++ backend/contracts/CreditManager.sol
@@ -5,21 +5,25 @@ pragma solidity ^0.8.28;
 This contract is to manage the carbon credit of an organization
 Act as a supply chain
 */
+import "./IERC20.sol";
 contract CreditManager {
     address public owner;
     uint256 public totalCredit;
     address[] public creditHolders;
     mapping(address => uint256) public creditBalance;
     mapping(address => bool) public holderList;
+    IERC20 public cct; // carbon credit token
 
-    event creditUsed(address holder, uint256 credit);
-
-    constructor(address _owner) {
+    event creditUsed(address indexed holder, address indexed recipient, uint256 credit);
+    event CreditAllocated(address indexed factory, uint256 amount);
+    constructor(address _owner,address _tokenAddress) {
         if (_owner == address(0)) {
             owner = msg.sender;
         } else {
             owner = _owner;
         }
+        require(_tokenAddress != address(0), "Invalid token address");
+        cct = IERC20(_tokenAddress);
     }
 
     modifier isOwner() {
@@ -44,6 +48,7 @@ contract CreditManager {
     
     function removeCreditHolder(address _holder) public isOwner {
         require(_holder != address(0), "Invalid address");
+        cct.approve(_holder, 0);
         for(uint i=0; i<creditHolders.length; i++) {
             if(creditHolders[i] == _holder) {
                 creditHolders[i] = creditHolders[creditHolders.length-1];
@@ -57,30 +62,34 @@ contract CreditManager {
     /**
     * @dev for owner to add credit to holder, and deduct it from total credit
     */
-    function giveCredit(address _holder, uint256 _credit) public isOwner {
+    function giveCredit(address _holder, uint256 _amount) public isOwner {
         require(_holder != address(0), "Invalid address");
-        require(totalCredit-_credit >= 0, "Insufficient credit");
-        totalCredit -= _credit;
-        creditBalance[_holder] += _credit; 
+        require(cct.balanceOf(address(this)) >= _amount, "Insufficient contract balance");
+        // Approve the factory to spend tokens on behalf of the contract
+        cct.approve(_holder, _amount);
     }
 
     /**
      * @dev for owner to reduce credit from holder, and add it back to total credit
      */
-    function reduceCredit(address _holder, uint256 _credit) public isOwner {
+    function reduceCredit(address _holder, uint256 _amount) public isOwner {
         require(_holder != address(0), "Invalid address");
-        require(creditBalance[_holder]>=_credit, "Insufficient credit");
-        creditBalance[_holder] -= _credit;
-        totalCredit += _credit;
+        uint256 currentAllowance = cct.allowance(address(this), _holder);
+        require(currentAllowance >= _amount, "Amount exceeds current allowance");
+
+        // Reduce the allowance by setting a new, lower value
+        cct.approve(_holder, currentAllowance - _amount);
     }
     /**
      * @dev for holders to use credit
      */
-    function useCredit(address _holder, uint256 _credit) public isHolder {
-        require(_holder != address(0), "Invalid address");
-        require(creditBalance[_holder]>= _credit , "Insufficient credit, your credit is ");
-        creditBalance[_holder] -= _credit;
-        emit creditUsed(_holder, _credit);
+    function useCredit(address _recipient,uint256 _amount) public isHolder {
+        require(_recipient != address(0), "Invalid recipient address");
+        require(cct.allowance(address(this), msg.sender) >= _amount, "Allowance insufficient");
+
+        // Factory spends tokens on behalf of the contract
+        cct.transferFrom(address(this), _recipient, _amount);
+        emit creditUsed(msg.sender,_recipient,_amount);
     }
     
     /**
@@ -88,16 +97,18 @@ contract CreditManager {
     * @param _creditPerHolder: amount of credit to be distributed to each holder
     */
     function distributeCredit(uint256 _creditPerHolder) public isOwner {
-        require(totalCredit-_creditPerHolder*creditHolders.length >= 0, "Insufficient credit");
-        for(uint i=0; i<creditHolders.length; i++) {
-            creditBalance[creditHolders[i]] += _creditPerHolder;
+        require(cct.balanceOf(address(this)) >= _creditPerHolder * creditHolders.length, "Insufficient token balance");
+        for (uint i = 0; i < creditHolders.length; i++) {
+            cct.approve(creditHolders[i], _creditPerHolder);
         }
-    }
-
-    function gainCredit(uint256 _credit) public isOwner {
-        // not complete, future implementation will be added here, e.g., credit from carbon ERC20 token
-        totalCredit += _credit;
     }
     
 
+    function topUp(uint256 _amount) public isOwner {
+        cct.transfer(address(this), _amount);
+    }
+
+    function getFactoryAllowance(address _factory) external view returns (uint256) {
+        return cct.allowance(address(this), _factory);
+    }
 }
