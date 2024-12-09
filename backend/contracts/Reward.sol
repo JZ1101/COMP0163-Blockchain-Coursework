9767f3c2904e226a6560fd66d4f5861cb928a83a
Jianian Zheng <jnz7276@gmail.com>
2024-12-09 13:09:03 +0000

    Updated reward contract

--- backend/contracts/Reward.sol
+++ backend/contracts/Reward.sol
@@ -1,5 +1,16 @@
 pragma solidity ^0.8.28;
 
+import "./IERC20.sol";
+/*
+@title Reward contract
+This contract is to reward the factory for being eco-friendly
+*/
 contract Reward{
-    
+    modifier ecoFriendly(address _factory){
+        // if usage shows a decrease in carbon emission
+        _;
+    }
+    giveReward(address _factory) public ecoFriendly(_factory){
+        // give reward to factory
+    }
 }
