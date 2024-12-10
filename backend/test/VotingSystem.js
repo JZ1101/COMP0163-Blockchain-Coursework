d4a587af3c55a4975f5f4dac8b2231beedf9f79f
Bingfan Xu <ec21635@qmul.ac.uk>
2024-12-10 18:00:44 +0000

    Update VotingSystem.js

--- backend/test/VotingSystem.js
+++ backend/test/VotingSystem.js
@@ -61,6 +61,52 @@ describe("Voting Contract", function () {
             .withArgs(addr1.address, 0, ethers.utils.parseEther("5"));
     });
 
+    it("Should allow users to vote for AMM contract", async function () {
+        await voteToken.mint(addr1.address, ethers.utils.parseEther("10"));
+        await voteToken.connect(addr1).approve(voting.address, ethers.utils.parseEther("1"));
+        await voting.connect(addr1).voteForAMMContract();
+        const totalVotesAMM = await voting.total_Votes_AMM();
+        expect(totalVotesAMM).to.equal(1);
+    });
+
+    it("Should allow users to vote for Supply Chain contract", async function () {
+        await voteToken.mint(addr1.address, ethers.utils.parseEther("10"));
+        await voteToken.connect(addr1).approve(voting.address, ethers.utils.parseEther("1"));
+        await voting.connect(addr1).voteForSupplyChainContract();
+        const totalVotesSupplyChain = await voting.total_Votes_SupplyChain();
+        expect(totalVotesSupplyChain).to.equal(1);
+    });
+
+    it("Should allow users to vote for Reward contract", async function () {
+        await voteToken.mint(addr1.address, ethers.utils.parseEther("10"));
+        await voteToken.connect(addr1).approve(voting.address, ethers.utils.parseEther("1"));
+        await voting.connect(addr1).voteForRewardContract();
+        const totalVotesReward = await voting.total_VotesReward();
+        expect(totalVotesReward).to.equal(1);
+    });
+
+    it("Should update system contracts", async function () {
+        await voting.updateSystemContracts(0, addr2.address);
+        const updatedContract = await voting.systemContracts(0);
+        expect(updatedContract).to.equal(addr2.address);
+    });
+
+    it("Should change owner", async function () {
+        await voting.changeOwner(addr1.address);
+        const newOwner = await voting.owner();
+        expect(newOwner).to.equal(addr1.address);
+    });
+
+    it("Should not allow non-owner to update system contracts", async function () {
+        await expect(voting.connect(addr1).updateSystemContracts(0, addr2.address))
+            .to.be.revertedWith("Only owner can call this function");
+    });
+
+    it("Should not allow non-owner to change owner", async function () {
+        await expect(voting.connect(addr1).changeOwner(addr2.address))
+            .to.be.revertedWith("Only owner can call this function");
+    });
+
     it("Should not allow voting without sufficient tokens", async function () {
         // Attempt to vote without tokens
         await expect(voting.connect(addr1).vote(0, ethers.utils.parseEther("5")))
