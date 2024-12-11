e139e31fb70a9db9d223147a4171ab6cd0b1f9b4
Jianian Zheng <jnz7276@gmail.com>
2024-12-11 12:56:27 +0000

    Update CreditManager.sol

--- backend/contracts/CreditManager.sol
+++ backend/contracts/CreditManager.sol
@@ -19,7 +19,9 @@ contract CreditManager {
     uint256 public periodCount = 0; // to track the period
     uint256[] yearlyUsageData; // usage data for each year
     uint256 calculationFlag = 0; // to aviod double calculation of the reward
+    mapping(address => uint256) cBalances; // credit balance 
 
+    mapping(address => mapping(address => uint256)) cAllowances;  // credit allowances   
     //reward claim
     uint256 public rewardClaimCounter = 0;
 
@@ -30,10 +32,17 @@ contract CreditManager {
 
     event creditUsed(address indexed holder, address indexed recipient, uint256 credit);
     event CreditAllocated(address indexed factory, uint256 amount);
-    event CreditDeallocated(address indexed factory, uint256 amount);// credit has been deallocated to x amount
+    event CreditDeallocated(address indexed factory, uint256 amount);// credit has been reduced x amount, credit -= x
     event TopUpCredit(address indexed this_contract, uint256 amount);// balance += amount
     event UpdateYearlyUsageData(uint256 totalUsage);
     event UpdateRCCounter(uint256 rewardClaimCounter);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event TransferOnBehalf(address indexed _from, address indexed _to, uint256 _value);
+    event Transfer(address indexed _to, uint256 _value);
+    event SetCredit(address indexed _holder, uint256 _value);
+    event AddCreditHolder(address indexed _holder);
+    event RemoveCreditHolder(address indexed _holder);
+    event ChangeOwner(address indexed _owner);
     
     constructor(address _owner, address _tokenAddress, address _rewardToken) {
         if (_owner == address(0)) {
@@ -64,12 +73,14 @@ contract CreditManager {
 
     function changeOwner(address _owner) public isOwner {
         owner = _owner;
+        emit ChangeOwner(owner);
     }
 
     function addCreditHolder(address _holder) public isOwner {
         require(_holder != address(0), "Invalid address");
         creditHolders.push(_holder);
         holderList[_holder] = true;
+        emit AddCreditHolder(_holder);
     }
     
     /**
@@ -79,7 +90,7 @@ contract CreditManager {
      */
     function removeCreditHolder(address _holder) public isOwner {
         require(_holder != address(0), "Invalid address");
-        cct.approve(_holder, 0);
+        cApprove(_holder, 0);
         for(uint i=0; i<creditHolders.length; i++) {
             if(creditHolders[i] == _holder) {
                 creditHolders[i] = creditHolders[creditHolders.length-1];
@@ -88,16 +99,17 @@ contract CreditManager {
             }
         }
         holderList[_holder] = false;
+        emit RemoveCreditHolder(_holder);
     }
 
     /**
-    * @dev for owner to add credit to holder, and deduct it from total credit
+    * @dev for owner to add credit to holder
     */
     function giveCredit(address _holder, uint256 _amount) public isOwner {
         require(_holder != address(0), "Invalid address");
         require(cct.balanceOf(address(this)) >= _amount, "Insufficient contract balance");
         // Approve the factory to spend tokens on behalf of the contract
-        cct.approve(_holder, _amount);
+        cApprove(_holder, _amount);// in scope of this supply chain
         emit CreditAllocated(_holder, _amount);
     }
 
@@ -108,51 +120,75 @@ contract CreditManager {
      */
     function reduceCredit(address _holder, uint256 _amount) public isOwner {
         require(_holder != address(0), "Invalid address");
-        uint256 currentAllowance = cct.allowance(address(this), _holder);
+        uint256 currentAllowance = allowanceOf(address(this), _holder);
         require(currentAllowance >= _amount, "Amount exceeds current allowance");
 
         // Reduce the allowance by setting a new, lower value
-        cct.approve(_holder, currentAllowance - _amount);
+        cApprove(_holder, currentAllowance - _amount);
         emit CreditDeallocated(_holder, _amount);
     }
+    /**
+     * @dev for owner to set credit to holder
+     * @param _holder address of the holder to set credit to
+     * @param _amount amount of credit to set to, e.g., 5 means credit = 5
+     */
+    function setCredit(address _holder, uint256 _amount) public isOwner {
+        require(_holder != address(0), "Invalid address");
+
+        // Reduce the allowance by setting a new, lower value
+        cApprove(_holder,  _amount);
+        emit SetCredit(_holder, _amount);
+    }
+    /**
+     * @dev for contract manager to transfer credit on behalf of factory
+     */
+    function transferOnBehalf(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool) {
+        require(cct.balanceOf(address(this)) >=_value,"not sufficient balance");
+        require(cAllowances[_from][msg.sender] >=_value,"not sufficient balance");
+        cct.transfer(_to,_value);
+        emit TransferOnBehalf(_from,_to,_value);
+        emit Transfer (_to,_value);
+        return true;
+
+    }
+    /**
+     * @dev for contract manager to approve credit to factory
+     */
+    function cApprove(address _spender, uint256 _value) public returns (bool) {
+        cAllowances[msg.sender][_spender]=_value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    /**
+     * @dev for holders to check their credit balance
+     */
+    function allowanceOf(address _owner, address _spender)
+        public
+        view
+        returns (uint256 remaining)
+    {
+        return cAllowances[_owner][_spender];
+    }   
+
     /**
      * @dev for holders to use credit
      * virtual function can connect to IOT device to deduct credit
      */
+
     function useCredit(address _recipient, uint256 _amount) public isHolder {
         require(_recipient != address(0), "Invalid recipient address");
+        require(cAllowances[address(this)][msg.sender] >= _amount, "Allowance insufficient");
 
-        // Data encoding for transferFrom
-        bytes memory data = abi.encodeWithSignature(
-            "transferFrom(address,address,uint256)",
-            address(this),    // From: contract
-            _recipient,       // To: recipient
-            _amount           // Amount
-        );
-
-        // Delegate the call to the ERC-20 contract
-        (bool success, ) = address(cct).delegatecall(data);
-
-        require(success, "Token transfer failed");
-
-        // Log the usage
+        // Factory spends tokens on behalf of the contract
+        transferOnBehalf(address(this), _recipient, _amount);
+        // cct.transferFrom(address(this), _recipient, _amount);
         usageData.push(OnTimeUsage(_amount, block.timestamp));
         emit creditUsed(msg.sender, _recipient, _amount);
     }
-
-    // function useCredit(address _recipient, uint256 _amount) public isHolder {
-    //     require(_recipient != address(0), "Invalid recipient address");
-    //     require(cct.allowance(address(this), msg.sender) >= _amount, "Allowance insufficient");
-
-    //     // Factory spends tokens on behalf of the contract
-    //     (bool success, ) = address(cct).call(
-    //     abi.encodeWithSignature("transferFrom(address(this), _recipient, _amount)", _recipient, _amount)
-    // );
-    //     require(success, "Token transfer failed");
-    //     // cct.transferFrom(address(this), _recipient, _amount);
-    //     usageData.push(OnTimeUsage(_amount, block.timestamp));
-    //     emit creditUsed(msg.sender, _recipient, _amount);
-    // }
     
     /**
     * @dev for owner to distribute credit to all holders
@@ -161,7 +197,7 @@ contract CreditManager {
     function distributeCredit(uint256 _creditPerHolder) public isOwner {
         require(cct.balanceOf(address(this)) >= _creditPerHolder * creditHolders.length, "Insufficient token balance");
         for (uint i = 0; i < creditHolders.length; i++) {
-            cct.approve(creditHolders[i], _creditPerHolder);
+            cApprove(creditHolders[i], _creditPerHolder);
             emit CreditAllocated(creditHolders[i], _creditPerHolder);
         }
     }
@@ -173,7 +209,7 @@ contract CreditManager {
     }
 
     function getFactoryAllowance(address _factory) external view returns (uint256) {
-        return cct.allowance(address(this), _factory);
+        return allowanceOf(address(this), _factory);
     }
     
 
@@ -215,5 +251,6 @@ contract CreditManager {
         require(success,"Reward claim failed");
 
     }
+
 
 }
